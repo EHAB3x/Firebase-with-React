0c65d03d2149c4394909b793b9cf2a23277f3710
EHAB3x <ehabmah6969@gmail.com>
2024-02-05 21:44:23 +0200

    styling components and re-build item-list component

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import "./ItemList.css"
 import {useFirestore} from '../../Firebase/useFirestore'
-import Item from '../item/Item';
 
 const ItemList = () => {
-  const {items} = useFirestore();
+  const {items, deleteItem} = useFirestore();
   return (
     <div className='item-list'>
-        {items.map(item => {
-          return <Item item={item} key={item.id}/>
-        })}
+        <table width="100%">
+            <thead>
+              <tr>
+                <td>Title</td>
+                <td>Amount</td>
+                <td>Date</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map(({title,amount,date,id})=>(
+                <tr key={id} className='white'>
+                  <td>{title}</td>
+                  <td className={amount >= 0 ? "income": "expense"}>${Math.abs(amount)}</td>
+                  <td>{date}</td>
+                  <td><button onClick={()=>deleteItem(id)}>Delete</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
     </div>
   )
 }
